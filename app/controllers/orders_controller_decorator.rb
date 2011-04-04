5ffb989d101d9210eddf34abc4d226ca93b9fd56
Roman Smirnov <roman@railsdog.com>
2011-04-04 16:58:51 +0400

    Added optional validation to restrict shipping estimation to work only with US zip codes.

--- app/controllers/orders_controller_decorator.rb
+++ app/controllers/orders_controller_decorator.rb
@@ -15,12 +15,14 @@ OrdersController.class_eval do
   end
   
   def estimate_shipping_cost
-    @order = current_order
-    @order.ship_address = Address.new(:zipcode => params[:zipcode], :country_id => Spree::Config[:default_country_id])
-
-    @shipping_methods = ShippingMethod.all_available(@order)
-    
-    @esc_values = @shipping_methods.map {|sm| [sm.name, sm.calculator.compute(@order)]}
+    if params[:zipcode] =~ /^\d{5}$/ || Spree::AdvancedCart::Config[:skip_zipcode_validation]
+      @order = current_order
+      @order.ship_address = Address.new(:zipcode => params[:zipcode], :country_id => Spree::Config[:default_country_id])
+      @shipping_methods = ShippingMethod.all_available(@order)    
+      @esc_values = @shipping_methods.map {|sm| [sm.name, sm.calculator.compute(@order)]}
+    else
+      flash[:error] = I18n.t(:estimation_works_only_with_us_zipcodes)
+    end
   end
   
 end
--- lib/advanced_cart_configuration.rb
+++ lib/advanced_cart_configuration.rb
@@ -1,4 +1,5 @@
 class AdvancedCartConfiguration < Configuration
   preference :enable_coupon_applying, :boolean, :default => true
   preference :enable_shipping_cost_calculation, :boolean, :default => true
+  preference :skip_zipcode_validation, :boolean, :default => false
 end
