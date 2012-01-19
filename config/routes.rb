cfc637f6b80ab063161c129b200093c537427400
Alexander Balashov <divineforest@gmail.com>
2012-01-19 19:30:47 +0400

    spree 1.0 main compatibility fixes
    * OrdersController and it's views moved to 'spree' namespace
    * new routes
    * order_price helper renamd to order_subtotal
    * new configuration API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
-Rails.application.routes.draw do
-  match "/estimate_shipping_cost", :to => 'orders#estimate_shipping_cost', :via => :get, :as => :estimate_shipping_cost
+Spree::Core::Engine.routes.append do
+
+  get "/estimate_shipping_cost", :to => 'orders#estimate_shipping_cost', :as => :estimate_shipping_cost
+
 end
+
--- lib/spree/advanced_cart_configuration.rb
+++ lib/spree/advanced_cart_configuration.rb
@@ -1,4 +1,4 @@
-class AdvancedCartConfiguration < Configuration
+class Spree::AdvancedCartConfiguration < Spree::Preferences::Configuration
   preference :enable_coupon_applying, :boolean, :default => true
   preference :enable_shipping_cost_calculation, :boolean, :default => true
   preference :skip_zipcode_validation, :boolean, :default => false
