465d6eca31cfcbaaa5b04f34d50f760f9e9f4152
Denis Ivanov <visible.h4x@gmail.com>
2013-07-19 20:53:29 -0700

    Removing ZIP code validation

--- app/controllers/orders_controller_decorator.rb
+++ app/controllers/orders_controller_decorator.rb
@@ -32,40 +32,30 @@ Spree::OrdersController.class_eval do
   def estimate_shipping_cost
     @order = current_order(true)
     # default attributes for stub address
+    country_id = params[:country_id] || Spree::Config[:default_country_id]
     address_attrs = { :zipcode => params[:zipcode], 
-                      :country_id => params[:country_id] || Spree::Config[:default_country_id] }
+                      :country_id => country_id }
     
-    zipcode_is_valid = true
-    if !Spree::AdvancedCart::Config[:skip_zipcode_validation]
-      # validate zipcode and override attributes of stub address    
-      if params[:zipcode] =~ /^\d{5}$/ # USA zipcode
-        state = state_id_by_zip(params[:zipcode])
-        address_attrs.merge!(:state_id => state.try(:id))
-      elsif params[:zipcode] =~ /[a-z]\d[a-z]\s?\d[a-z]\d/i # Canadian zipcode
-        address_attrs.merge!(:country_id => Spree::Country.find_by_iso('CA').try(:id))
-      else
-        zipcode_is_valid = false
-      end
+    country = Spree::Country.find(country_id)
+    if country.iso == 'US'
+      state = state_id_by_zip(params[:zipcode])
+      address_attrs.merge!(:state_id => state.try(:id))
     end
-    
-    if zipcode_is_valid
-      @order.ship_address = Spree::Address.new(address_attrs)
-      @shipping_methods = Spree::ShippingMethod.all_available(@order)    
-      @esc_values = @shipping_methods.map do |sm|
-          error = nil
-          begin
-            rate = sm.calculator.compute(@order) 
-          rescue Spree::ShippingError => ex
-            error = ex.message.sub("Shipping Error: ", '')
-          rescue Net::HTTPServiceUnavailable
-            error = nil # carrier is unavailable atm, don't show this shipping method in the list
-          end
-          [sm.name, rate, error]
-        end.select{|sm| sm[1] || sm[2]}. # a shipping calculator can return nil for the price if error occurred
-            sort_by{|sm| sm[1] ? sm[1] : Float::MAX} # mimic default spree_core behavior, preserve asc cost order
-    else
-      flash[:error] = I18n.t('estimation_requires_supported_zipcode')
-    end
+
+    @order.ship_address = Spree::Address.new(address_attrs)
+    @shipping_methods = Spree::ShippingMethod.all_available(@order)    
+    @esc_values = @shipping_methods.map do |sm|
+        error = nil
+        begin
+          rate = sm.calculator.compute(@order) 
+        rescue Spree::ShippingError => ex
+          error = ex.message.sub("Shipping Error: ", '')
+        rescue Net::HTTPServiceUnavailable
+          error = nil # carrier is unavailable atm, don't show this shipping method in the list
+        end
+        [sm.name, rate, error]
+      end.select{|sm| sm[1] || sm[2]}. # a shipping calculator can return nil for the price if error occurred
+          sort_by{|sm| sm[1] ? sm[1] : Float::MAX} # mimic default spree_core behavior, preserve asc cost order
     
     respond_with do |format|
       format.html do
