5836afd910c8f00ca46599d9eafa0aa90ca1366b
Roman Smirnov <POMAHC@gmail.com>
2014-01-29 22:01:49 +0400

    Minor fix of estimate_shipping_cost route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Spree::Core::Engine.routes.append do
 
-  get "/estimate_shipping_cost", :to => 'orders#estimate_shipping_cost', :as => :estimate_shipping_cost
+  get "/estimate_shipping_cost", :to => 'orders#estimate_shipping_cost'
 
 end
-
